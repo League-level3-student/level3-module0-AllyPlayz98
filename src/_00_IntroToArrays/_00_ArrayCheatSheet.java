1a05fc2115140a46813065bc2fce0d0459048d04
root <root@cd78aed8941a>
2020-09-21 23:56:16 +0000

    september 21, 2020

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -16,20 +16,32 @@ public class _00_ArrayCheatSheet {
 		// choice
 		String b = new String("hey");
 		for (int a = 0; a < array1.length; a++) {
-			array1[array1.length] = b;
+			array1[a] = b;
 		}
 		// 6. use a for loop to print all the values in the array
 		// BE SURE TO USE THE ARRAY'S length VARIABLE
 		for (int c = 0; c < array1.length; c++) {
-			System.out.println(array1.length);
+			System.out.println(array1[c]);
 		}
 		// 7. make an array of 50 integers
 		int[] array2 = new int[50];
 		// 8. use a for loop to make every value of the integer array a random number
+Random r = new Random();
 
+for (int j = 0; j < array2.length; j++) {
+	int i = r.nextInt();
+	array2[j] = i;
+	System.out.println(array2[j]);
+}
 		// 9. without printing the entire array, print only the smallest number on the
 		// array
-
+int smallest = array2[0];
+		for (int i = 0; i < array2.length; i++) {
+			if(smallest > array2[i]) {
+				smallest = array2[i];
+				System.out.println(smallest);
+			}
+		}
 		// 10 print the entire array to see if step 8 was correct
 
 		// 11. print the largest number in the array.
